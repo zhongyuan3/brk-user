fee66cc277c0b817cf49dd67aafef6eba0989e83
zhongyuan3 <zhongyuanpan3@gmail.com>
2026-05-07 17:56:01 +0800

    fix: bugs in echo.c that loops printing argv[1]
    In the loop, argv[1] always prints. Print argv[i] instead.

--- src/echo.c
+++ src/echo.c
@@ -1,17 +1,16 @@
-#include <string.h>
-#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
-		write(STDOUT_FILENO, argv[1], strlen(argv[1]));
+	int i = 1;
 
-	for (int i = 2; i < argc; ++i) {
-		write(STDOUT_FILENO, " ", 1);
-		write(STDOUT_FILENO, argv[1], strlen(argv[1]));
+	if (i < argc) {
+		printf("%s", argv[i]);
+		++i;
+		for (; i < argc; ++i)
+			printf(" %s", argv[i]);
 	}
-
-	write(STDOUT_FILENO, "\n", 1);
+	putchar('\n');
 
 	return 0;
 }
