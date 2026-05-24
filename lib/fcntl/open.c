c2a76e34ac35933f1befa14ba9ae41acc63fff7d
zhongyuan3 <zhongyuanpan3@gmail.com>
2026-05-24 11:52:50 +0800

    feat: open(O_CREAT)

--- lib/fcntl/open.c
+++ lib/fcntl/open.c
@@ -1,7 +1,16 @@
+#include <stdarg.h>
+#include <sys/types.h>
 #include <syscall.h>
 #include <fcntl.h>
 
 int open(const char *path, int flags, ...)
 {
-	return syscall(SYS_open, path, flags, 0);
+	va_list ap;
+	mode_t mode;
+
+	va_start(ap, flags);
+	mode = va_arg(ap, mode_t);
+	va_end(ap);
+
+	return syscall(SYS_open, path, flags, mode);
 }
